16db0c8fdbd8d5418a584c65afc79a5e94e5d25e
Ladislav Trupl <ladist@gmail.com>
2020-02-14 18:32:31 -0300

    apos add --all

--- README.md
+++
@@ -1,2 +0,0 @@
-# website
-aula website com slim
---
+++ index.php
@@ -0,0 +1,17 @@
+<?
+require_once("vendor/autoload.php");
+
+$app=new \Slim\Slim();
+
+$app->get('/', function(){
+    echo "Home Page";
+});
+
+$app->get('/hello/:name', function($name){
+    echo "Alô, ".$name;
+});
+
+$app->run();
+
+
+?>
